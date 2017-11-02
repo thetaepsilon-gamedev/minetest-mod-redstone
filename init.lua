e352d8b824010bcd9f5cbafe6c1359db978abe75
thetaepsilon-gamedev <thetaepsilon-gamedev@noreply.users.github.com>
2017-11-02 12:45:49 +0000

    add redstone lump item

--- init.lua
+++ init.lua
@@ -1,11 +1,18 @@
 local tiles = { "default_stone.png^redstone_redstone_overlay.png" }
 local orename = "redstone:stone_with_redstone"
+local lumpname = "redstone:redstone_lump"
+
+minetest.register_craftitem(lumpname, {
+	description = "Redstone lump",
+	inventory_image = "redstone_redstone_lump.png",
+})
 
 minetest.register_node(orename, {
 	description="Redstone ore",
 	tiles = tiles,
 	groups = { cracky=1 },
 	sounds = default.node_sound_stone_defaults(),
+	drop = lumpname,
 })
 
 local b = 13
@@ -19,3 +26,5 @@ minetest.register_ore({
 	y_min		= -31000,
 	y_max		= -64,
 })
+
+
